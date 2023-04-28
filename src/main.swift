4e0cff758e3f68b2abf12b7656b31bb07db247fd
yuntianw129 <yuntianw@student.unimelb.edu.au>
2023-04-28 21:20:39 +1000

    test

---
+++ src/main.swift
@@ -0,0 +1,24 @@
+// main.swift
+
+import Foundation
+
+class Person {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+
+    func introduce() {
+        print("Hi, my name is \(name) and I am \(age) years old.")
+    }
+}
+
+func main() {
+    let person = Person(name: "Alice", age: 30)
+    person.introduce()
+}
+
+main()
